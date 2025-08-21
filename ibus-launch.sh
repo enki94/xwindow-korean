cb07121efe68df61634cbd2e85d50e203e6a2c4f
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-08-21 16:06:09 +0900

    Update ibus-launch.sh

--- ibus-launch.sh
+++ ibus-launch.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 export XDG_RUNTIME_DIR="/tmp/ibus-runtime"
 mkdir -p "$XDG_RUNTIME_DIR"
 chmod 700 "$XDG_RUNTIME_DIR"
@@ -9,4 +7,5 @@ export QT_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 
 ibus-daemon -drx
+sleep 1
 ibus engine hangul
