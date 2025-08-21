170c2e5e6db6c8ca82502a28c8e5b5475c4aa4d5
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-08-21 17:08:46 +0900

    Update and rename ibus-launch.sh to first_login_setup.sh

--- first_login_setup.sh
+++ first_login_setup.sh
@@ -1,11 +1,9 @@
-export XDG_RUNTIME_DIR="/tmp/ibus-runtime"
-mkdir -p "$XDG_RUNTIME_DIR"
-chmod 700 "$XDG_RUNTIME_DIR"
-
 export GTK_IM_MODULE=ibus
 export QT_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 
+xdg-settings set default-web-browser chromium.desktop
+
 ibus-daemon -drx
-sleep 1
+sleep 5
 ibus engine hangul
