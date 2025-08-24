1ff3c49f86344860bfbc3a4c34552cbc6981d366
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2025-08-24 22:54:47 +0900

    Update user-entrypoint.sh

--- user-entrypoint.sh
+++ user-entrypoint.sh
@@ -31,7 +31,4 @@ fi
 
 # normal start scripts ------------------------------------------------------------------------
 fcitx5 -d
-sleep 5
-xmodmap -e "keycode 121 = Hangul_Hanja"
-xmodmap -e "keycode 122 = Hangul"
-xmodmap -e "keycode 123 = Hangul_Hanja"
+(sleep 5; xmodmap -e "keycode 121 = Hangul_Hanja"; xmodmap -e "keycode 122 = Hangul"; xmodmap -e "keycode 123 = Hangul_Hanja";) &
